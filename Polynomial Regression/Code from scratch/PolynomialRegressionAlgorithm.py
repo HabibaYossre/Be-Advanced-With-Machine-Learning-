fe0126b59af083d53fe6fc13573de5aebb865559
HabibaYossre <141557659+HabibaYossre@users.noreply.github.com>
2024-03-19 14:54:59 +0200

    Add files via upload

---
+++ Polynomial Regression/Code from scratch/PolynomialRegressionAlgorithm.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#class Polynomial_Regression:
+global losses    
+losses = []
+def loss_function(y_true, y_hat):
+        loss = np.mean((y_hat - y_true)**2)
+        return loss
+
+def calculate_gradients(X, y_true, y_pred):
+        num_rows = X.shape[0]
+        dw = (1/num_rows)*np.dot(X.T, (y_pred - y_true))
+        db = (1/num_rows)*np.sum((y_pred - y_true))
+        return dw, db
+
+def create_polynomial_feature_set(X, degrees):
+        t = X.copy()
+        for i in degrees:
+            X = np.append(X, t**i, axis=1)
+        return X
+
+def train(X, y, batch_size, degrees, epochs, lr):
+        x = create_polynomial_feature_set(X, degrees)
+        m, n = x.shape
+        w = np.zeros((n,1))
+        b = 0
+        y = y.reshape(m,1)
+        
+        for epoch in range(epochs):
+            for i in range((m-1)//batch_size + 1):
+                start_i = i*batch_size
+                end_i = start_i + batch_size
+                x_batch = x[start_i:end_i]
+                y_batch = y[start_i:end_i]
+                y_hat = np.dot(x_batch, w) + b
+                dw, db = calculate_gradients(x_batch, y_batch, y_hat)
+                w -= lr*dw
+                b -= lr*db
+            l = loss_function(y, np.dot(x, w) + b)
+            losses.append(l)
+        return w, b, losses
+
+def predict(X, w, b, degrees):
+        x1 = create_polynomial_feature_set(X, degrees)
+        return np.dot(x1, w) + b
+
+def r2_score(y, y_hat):
+        sse = np.sum((np.array(y_hat)-np.array(y))**2)
+        tss = np.sum((np.array(y)-np.mean(np.array(y)))**2)
+        return 1 - (sse / tss )
